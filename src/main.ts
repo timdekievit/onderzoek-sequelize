c01c2e9acdcb77f884bbbad85813d22bd6fb7fc4
s1102579 <s1102579@student.hsleiden.nl>
2022-02-01 19:01:49 +0100

    w.i.p

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppService } from './app.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const appService = new AppService();
+  await appService.seedUsers();
   await app.listen(3000);
 }
 bootstrap();
